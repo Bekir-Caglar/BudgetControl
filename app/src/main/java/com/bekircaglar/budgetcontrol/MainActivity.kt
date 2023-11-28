f3d66aa8478e23c06540b45dca338bbdc97bc662
Bekir Çağlar <uyibey@gmail.com>
2023-11-29 01:11:10 +0300

    BudgetControl v1

--- app/src/main/java/com/bekircaglar/budgetcontrol/MainActivity.kt
+++ app/src/main/java/com/bekircaglar/budgetcontrol/MainActivity.kt
@@ -2,10 +2,27 @@ package com.bekircaglar.budgetcontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.bekircaglar.budgetcontrol.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavBar = binding.bottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragmentContainerView
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavBar, navController)
+
     }
 }
